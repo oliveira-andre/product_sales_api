8e98f1c8269d67b90beeea901fdcac92865cc464
Andre Oliveira <desmonplays@gmail.com>
2020-07-06 15:14:16 -0400

    feature: getting all products by name and price of OLX

--- app/services/web_crawler_service.rb
+++ app/services/web_crawler_service.rb
@@ -1,5 +1,48 @@
 class WebCrawlerService
-  def initialize
+  require 'open-uri'
+
+  def initialize(query)
+    @config = Rails.application.config_for(:crawler)
+    @sites = @config[:sites]
+    @query = query
+    @products = []
+  end
+
+  def execute
+    @sites.each do |site|
+      @current_site = site.to_sym
+
+      cards.each do |card|
+        @card = card
+        @products << product_object unless name.blank?
+      end
+    end
+  end
+
+  private
+
+  def uri
+    @config.dig(@current_site, :uri) + @query
+  end
+
+  def page
+    Nokogiri::HTML(URI.open(uri))
+  end
+
+  def cards
+    page.search(@config.dig(@current_site, :cards))
+  end
+
+  def name 
+    @card.search(@config.dig(@current_site, :name)).text
+  end
+
+  def price
+    @card.search(@config.dig(@current_site, :price)).text
+  end
+
+  def product_object
+    { name: name, price: price, site: @current_site }.compact
   end
 end
 
