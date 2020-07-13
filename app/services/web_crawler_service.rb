75bb84d097502f511247628e1ba04139ef789bcc
Andre Oliveira <desmonplays@gmail.com>
2020-07-13 10:29:08 -0400

    feature: adding the deep, to getting deep into site pages if valid

--- app/services/web_crawler_service.rb
+++ app/services/web_crawler_service.rb
@@ -1,20 +1,27 @@
+# frozen_string_literal: true
+
+# this class open sites and return product_object
 class WebCrawlerService
   require 'open-uri'
 
-  def initialize(query)
+  def initialize(query, deep = 0)
     @config = Rails.application.config_for(:crawler)
     @sites = @config[:sites]
     @query = query
+    @deep = deep
     @products = []
   end
 
   def execute
     @sites.each do |site|
-      @current_site = site.to_sym
+      (0..@deep).each do |number|
+        @current_deep = number
+        @current_site = site.to_sym
+        set_card_and_product
 
-      cards.each do |card|
-        @card = card
-        @products << product_object unless name.blank?
+        return @products.uniq unless valid_next_page?
+
+        @next_page_uri = next_page_uri
       end
     end
   end
@@ -26,14 +33,25 @@ class WebCrawlerService
   end
 
   def page
-    Nokogiri::HTML(URI.open(uri))
+    Nokogiri::HTML(URI.open(@current_deep.zero? ? uri : @next_page_uri))
+  end
+
+  def set_card_and_product
+    cards.each do |card|
+      @card = card
+      @products << product_object unless name.blank?
+    end
   end
 
   def cards
     page.search(@config.dig(@current_site, :cards))
   end
 
-  def name 
+  def product_object
+    { name: name, price: price, site: @current_site }.compact
+  end
+
+  def name
     @card.search(@config.dig(@current_site, :name)).text
   end
 
@@ -41,8 +59,11 @@ class WebCrawlerService
     @card.search(@config.dig(@current_site, :price)).text
   end
 
-  def product_object
-    { name: name, price: price, site: @current_site }.compact
+  def next_page_uri
+    page.search(@config.dig(@current_site, :next_page))&.first&.attributes&.first&.last&.value
+  end
+
+  def valid_next_page?
+    @current_deep != @deep && next_page_uri&.present?
   end
 end
-
