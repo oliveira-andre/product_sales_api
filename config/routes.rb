fa7be63985700450047cc88c44479516f52167c7
Andre Oliveira <desmonplays@gmail.com>
2020-07-23 22:15:01 -0400

    fix: right params replaced on alexa_id

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :alexa, only: %i[], params: :skill_name do
+      resources :alexa, only: %i[], param: :skill_name do
         resources :skills, only: :index
       end
     end
