0f11ded6f10ba2f3ff3fa1c2111045c208417257
Andre Oliveira <desmonplays@gmail.com>
2020-07-26 18:40:03 -0400

    feature: getting serializer initializing data and catch serialize method

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -4,7 +4,8 @@ module Response
   def success_response(params = {})
     return unless params[:data] || params[:model]
 
-    render json: serializer(params[:model]).constantize.new(data).serialize,
+    render json: serializer(params[:model]).constantize.new(params[:data])
+                                           .serialize,
            status: status
   end
 
